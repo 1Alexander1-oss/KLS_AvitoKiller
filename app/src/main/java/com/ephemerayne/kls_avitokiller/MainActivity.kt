a459bae6cdb4e9bf436aa24532c30d9382931fe9
1Alexander1-oss <kolesnikov.1994@bk.ru>
2021-09-12 11:20:47 +0300

    Create Product Activity

--- app/src/main/java/com/ephemerayne/kls_avitokiller/MainActivity.kt
+++ app/src/main/java/com/ephemerayne/kls_avitokiller/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.ephemerayne.kls_avitokiller
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    lateinit var openAddProductScreen: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        openAddProductScreen = findViewById(R.id.open_add_product_screen)
+        openAddProductScreen.setOnClickListener {
+            val intent = Intent(this, AddProductActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
