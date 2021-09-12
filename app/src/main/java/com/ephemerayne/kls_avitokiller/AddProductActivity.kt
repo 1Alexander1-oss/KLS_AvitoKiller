0ec743329f4e959c17aba304d61eedfbfffaa0fc
1Alexander1-oss <kolesnikov.1994@bk.ru>
2021-09-12 13:01:43 +0300

    Create id for all views in AddProductActivity.kt

--- app/src/main/java/com/ephemerayne/kls_avitokiller/AddProductActivity.kt
+++ app/src/main/java/com/ephemerayne/kls_avitokiller/AddProductActivity.kt
@@ -1,17 +1,35 @@
 package com.ephemerayne.kls_avitokiller
 
+import android.location.Address
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 
 class AddProductActivity : AppCompatActivity() {
     lateinit var categoriesSpinner: Spinner
+    lateinit var productName: EditText
+    lateinit var productPrice: EditText
+    lateinit var productAddress: EditText
+    lateinit var description: EditText
+    lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_product)
         categoriesSpinner = findViewById(R.id.categories)
+        setCategoriesAdapter()
+        productName = findViewById(R.id.name)
+        productPrice = findViewById(R.id.price)
+        productAddress = findViewById(R.id.address)
+        description = findViewById(R.id.description)
+        button = findViewById(R.id.publish)
+
+    }
+
+    private fun setCategoriesAdapter() {
         val categories: Array<String> = arrayOf("еда", "животные", "авто")
 
         // Создаем адаптер ArrayAdapter с помощью массива строк и стандартной разметки элемета spinner
@@ -27,5 +45,4 @@ class AddProductActivity : AppCompatActivity() {
         // Применяем адаптер к элементу spinner
         categoriesSpinner.adapter = categoriesAdapter
     }
-
 }
